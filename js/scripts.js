60317fc2e641e1c4b4a82a5c6a6753bf0ca11edf
CardonEmily <141743610+CardonEmily@users.noreply.github.com>
2023-11-06 17:06:30 -0500

    Rewrote code into jQuery
    Edited the vanilla javascript to align with jQuery code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,30 @@
 function newItem() {
   //add new item to the bottom of the list
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("input").value;
-  const text = document.createTextNode(inputValue);
-  li.appendChild(text);
+  const li = $("<li></li>");
+  const inputValue = $("#input").val();
+  li.append(inputValue);
 
   if (inputValue === "") {
     alert("Don't forget to put in your task!");
   } else {
-    let list = document.querySelector("#list");
-    list.appendChild(li);
+    $("#list").append(li);
   }
 
   //cross out complete tasks
-  function crossOut() {
-    li.classList.toggle("strike");
-  }
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
 
-  li.addEventListener("dblclick", crossOut);
+  //add a button to remove items from list
+  const crossOutButton = $("<crossOutButton><crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
 
-  //add a button to remove items
-  const crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
-
-  crossOutButton.addEventListener("click", deleteListItem);
+  crossOutButton.on("click", deleteListItem);
 
   //class 'delete' from CSS
   function deleteListItem() {
-    li.classList.add("delete");
+    li.addClass("delete");
   }
 
   //make items able to be reorganized
